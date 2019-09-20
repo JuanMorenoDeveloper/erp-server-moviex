3690389387803f46e4eb05f8ede05f78a4ab429d
Juan M.vi <earth001@gmail.com>
2019-09-20 13:43:36 -0300

    UPDATE Actualización de configuracion de swagger

--- src/main/kotlin/uy/edu/ude/ErpMovieXServerApplication.kt
+++ src/main/kotlin/uy/edu/ude/ErpMovieXServerApplication.kt
@@ -4,10 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration
-import springfox.documentation.swagger2.annotations.EnableSwagger2
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc
 
 @SpringBootApplication
-@EnableSwagger2
+@EnableSwagger2WebMvc
 @Import(SpringDataRestConfiguration::class)
 class ErpMovieXServerApplication
 
